9c89a1fcd2c55a55f2aeb450eed41201424ddf17
Mohammed labeeb <labeebshanavas@gmail.com>
2020-05-29 12:01:33 +0530

    fixed bugs

--- app.js
+++ app.js
@@ -9,82 +9,90 @@ const homeStartingContent = "This is an application to note down minutes of busi
 
 const app = express();
 
-let postArray=[];
+let postArray = [];
 
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get('/',function(req,res){
-  res.render("home",{content:homeStartingContent, post:postArray,option:0});
+app.get('/', function (req, res) {
+  res.render("home", { content: homeStartingContent, post: postArray, option: 0 });
 });
 
-app.get('/rm',function(req,res){
-  res.render("home",{content:homeStartingContent, post:postArray,option:1});
+app.get('/rm', function (req, res) {
+  res.render("home", { content: homeStartingContent, post: postArray, option: 1 });
 })
 
-app.get('/compose',function(req,res){
+app.get('/compose', function (req, res) {
   res.render("compose");
- 
+
 })
 
-app.post('/compose',function(req,res){
-  const post= {
-    title: req.body.title,
-    postBody: req.body.post
+app.post('/compose', function (req, res) {
+  const post = {
+    date: req.body.Date,
+    Topic: req.body.Topic,
+    Attendees: req.body.Attendees,
+    Topic: req.body.Topic,
+    Decision: req.body.Decision,
+    nweek: req.body.nweek,
+    meet: req.body.meet,
+    post: req.body.post
+
   };
   postArray.push(post);
   res.redirect('/');
 });
 
-app.get('/posts/:topic',function(req,res){
-  postArray.forEach(function(element){
+app.get('/posts/:topic', function (req, res) {
+  postArray.forEach(function (element) {
 
-  const urlTopic = req.params.topic;
-  const titleTopic = element.title;
-  const titlebody = element.postBody;
+    const urlTopic = req.params.topic;
+    const titleTopic = element.Topic;
+    const titledate = element.date;
+    const attendees = element.Attendees;
+    const topic = element.Topic;
+    const decision = element.Decision;
+    const nweek = element.nweek;
+    const meet = element.meet;
+    const post = element.post;
 
-  const urlTopicLowered = _.lowerCase(urlTopic);
-  const titleTopicLowered = _.lowerCase(titleTopic);
+    const urlTopicLowered = _.lowerCase(urlTopic);
+    const titleTopicLowered = _.lowerCase(titleTopic);
 
-  if(urlTopicLowered==titleTopicLowered)
-  {
-    res.render("post.ejs",{heading:titleTopic, titleB: titlebody});
-  }
-  else
-  {
-    console.log("not found");
-  }
-  });
-  
-  
-});
-
-app.get('/remove/:topic',function(req,res){
-  postArray.forEach(function(element){
-
-  const urlTopic = req.params.topic;
-  const titleTopic = element.title;
-  const titlebody = element.postBody;
-
-  const urlTopicLowered = _.lowerCase(urlTopic);
-  const titleTopicLowered = _.lowerCase(titleTopic);
-
-  if(urlTopicLowered==titleTopicLowered)
-  {
-    const arrayIndex = postArray.indexOf(element);
-    if(arrayIndex>-1)
-    {
-      postArray.splice(arrayIndex,1);
+    if (urlTopicLowered == titleTopicLowered) {
+      res.render("post.ejs", { date: titledate, topic:topic, attendees: attendees, decision: decision, nweek: nweek, meet: meet, post: post });
     }
-    res.redirect('/');
-  }
-  else
-  {
-    console.log("not found");
-  }
+    else {
+      console.log("not found");
+    }
+  });
+
+
+});
+
+app.get('/remove/:topic', function (req, res) {
+  postArray.forEach(function (element) {
+
+    const urlTopic = req.params.topic;
+    const titleTopic = element.title;
+    const titlebody = element.postBody;
+
+    const urlTopicLowered = _.lowerCase(urlTopic);
+    const titleTopicLowered = _.lowerCase(titleTopic);
+
+    if (urlTopicLowered == titleTopicLowered) {
+      const arrayIndex = postArray.indexOf(element);
+      if (arrayIndex > -1) {
+        postArray.splice(arrayIndex, 1);
+      }
+      res.redirect('/');
+    }
+    else {
+      console.log("not found");
+    }
   });
 
 
@@ -94,6 +102,6 @@ app.get('/remove/:topic',function(req,res){
 
 
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started");
 });
